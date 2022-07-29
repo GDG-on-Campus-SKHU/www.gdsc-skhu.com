d303ffad4150f00c69f98ab87fcf8224fbe90c38
Hyesung Oh <haesungoh414@gmail.com>
2022-07-30 00:06:26 +0900

    env: setting compress, three, bundle-analyzer at next configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
+const withTM = require('next-transpile-modules')(['three']);
+const withPlugins = require('next-compose-plugins');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+const CompressionPlugin = require('compression-webpack-plugin');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack: config => {
+    config.plugins.push(new CompressionPlugin());
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = withPlugins([withTM, withBundleAnalyzer], nextConfig);
